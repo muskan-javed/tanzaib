35c21604dd6007ee28b50fe6be486f1895d17de7
Muskan Javed <64351437+muskan-javed@users.noreply.github.com>
2024-02-26 01:50:04 +0500

    change g tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={marcellus.className}>{children}</body>
-      <GoogleAnalytics gaId="G-XYZ" />
+      <GoogleAnalytics gaId="" />
     </html>
   )
 }
